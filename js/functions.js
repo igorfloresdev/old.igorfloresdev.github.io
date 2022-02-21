0b45a8cd1779fb521d98f4dcc2388650f7b56b0b
Igor Flores <dev.igorflores@gmail.com>
2022-02-21 20:10:01 -0300

    Criação de menu hamburguer responsivo

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,33 @@
+//Menu mobile
+
+const botao_menu = document.querySelector(".menu-mobile");
+const botao_fechar = document.querySelector(".fechar-mobile");
+const menu_mobile = document.querySelector(".header-menu ul");
+
+var tamanho_tela = window.innerWidth;
+
+window.addEventListener("resize", function () {
+  tamanho_tela = window.innerWidth;
+
+  if (tamanho_tela > 800) {
+    menu_mobile.style.display = "flex";
+  } else {
+    menu_mobile.style.display = "none";
+  }
+});
+
+if (tamanho_tela <= 800) {
+  botao_menu.onclick = function () {
+    menu_mobile.style.display = "grid";
+    botao_fechar.style.display = "initial";
+  };
+
+  botao_fechar.onclick = function () {
+    menu_mobile.style.display = "none";
+    botao_fechar.style.display = "none";
+  };
+}
+
 // Efeito de TipeWrite
 
 function typeWrite(elemento) {
