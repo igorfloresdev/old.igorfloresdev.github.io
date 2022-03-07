f3078221f7f587f8cf28c01277f8bb3af8d13053
Igor Flores <dev.igorflores@gmail.com>
2022-03-07 13:06:29 -0300

    adicionado evento de fechar ao clicar nos links dentro do menu mobile

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 //Menu mobile
 const botao_menu = document.querySelector(".menu-mobile");
 const botao_fechar = document.querySelector(".fechar-mobile");
+const menu_item = document.querySelectorAll(".header-menu ul li a");
 
 botao_menu.addEventListener("click", function () {
   const menu = document.querySelector(".header-menu ul");
@@ -11,6 +12,15 @@ botao_fechar.addEventListener("click", function () {
   const menu = document.querySelector(".header-menu ul");
   menu.classList.remove("mostrar");
 });
+
+const menu_itens = Array.from(menu_item);
+
+for (let i = 0; i < menu_item.length; i++) {
+  menu_itens[i].addEventListener("click", function () {
+    const menu = document.querySelector(".header-menu ul");
+    menu.classList.remove("mostrar");
+  });
+}
 
 // Efeito de TipeWrite
 
